e0d4fa11ec3a3f4515b2b4d2b2764658a50fb73e
Olivier Jacquemart <o.jacquemart@gmail.com>
2012-11-14 23:43:44 +0100

    Remove .worksheet dir.

--- .worksheet/src/models.test.scala
+++
@@ -1,5 +0,0 @@
-package models
-
-object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(85); val res$0 = 
-  "00 34 43".replaceAll("(\\d)\\s?", "<span>$1</span>");System.out.println("""res0: java.lang.String = """ + $show(res$0))}
-}
